c809755a5bd41ff3e2087646025daad6f96ab847
Daeng Coding <59271775+rintwo512@users.noreply.github.com>
2022-11-07 23:58:09 +0800

    2022/11/7

--- new code.ino
+++ new code.ino
@@ -1,27 +1,14 @@
-/*************************************************************
 
-  This is a simple demo of sending and receiving some data.
-  Be sure to check out other examples!
- *************************************************************/
-
-// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
-// See the Device Info tab, or Template settings
 #define BLYNK_TEMPLATE_ID "TMPL1ov1xbJZ"
 #define BLYNK_DEVICE_NAME "Smart Home"
 #define BLYNK_AUTH_TOKEN "bG6ROuRQ4qPRS_3TOy3l5__kOvtGR7-4"
-
-
-// Comment this out to disable prints and save space
 #define BLYNK_PRINT Serial
-
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
 char auth[] = BLYNK_AUTH_TOKEN;
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
 char ssid[] = "RH";
 char pass[] = "makassar92";
 
@@ -37,17 +24,13 @@ BlynkTimer timer;
 WidgetLED led1(V6);
 WidgetLCD lcd(V7);
 
-
 long duration;
 long distance;
 
-
 void setup()
 {
-  // Debug console
   Serial.begin(115200);
-
-  
+ 
   pinMode(2, OUTPUT); // Initialise digital pin 2 as an output pin
   pinMode(0, OUTPUT); // Initialise digital pin 2 as an output pin
 
@@ -66,19 +49,18 @@ void setup()
 }
 
 
-BLYNK_WRITE(V0) // Executes when the value of virtual pin 0 changes
-{
+BLYNK_WRITE(V0) // PIN LAMPU BLYNK
     if (param.asInt() == 1)
     {        
-        digitalWrite(2, LOW); // Set digital pin 2 LOW
+        digitalWrite(2, LOW);
     }
     else
     {        
-        digitalWrite(2, HIGH); // Set digital pin 2 HIGH
+        digitalWrite(2, HIGH);
     }
 }
 
-BLYNK_WRITE(V1)
+BLYNK_WRITE(V1) //PIN STOP KONTAK BLYNK 
 {
     if (param.asInt() == 1)
     {      
@@ -90,7 +72,7 @@ BLYNK_WRITE(V1)
     }
 }
 
-BLYNK_WRITE(V4)
+BLYNK_WRITE(V4) // PIN TOMBOL MONITOR JARAK BLYNK
 {
     if (param.asInt() == 1)
     {    
@@ -109,120 +91,91 @@ void loop() {
 
   Blynk.run();
   if (digitalRead(LEDsystem) == HIGH)  {
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
     
-    //Set the trigPin on HIGH state for 10 micro seconds
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    
-    //Read the echoPin, return to sound wave travel time to microseconds
-    duration = pulseIn(echoPin, HIGH);
-  
-    //Calculating the distance
-    distance = duration*0.034/2; //s=t*v
-  
-    //kirim data ke blynk
-    Blynk.virtualWrite(V5, distance);   
-
-    //LCD Di Blynk    
-    
-    lcd.print (3, 0, "Makassar012");
-    // lcd.print (0, 1, "Jarak : " + String(distance) + " cm");
-    lcd.print (1, 1, "Smart Home 101");
-    timer.run();
-    logic();
-
-    Serial.print("Durasi: ");
-    Serial.println(duration);
-    Serial.print("Jarak: ");
-    Serial.print(distance);
-    Serial.print (" cm");
-    Serial.println (" ");
-    delay(100);
-  }
-  else  {
-    digitalWrite(LEDsystem, LOW); //pembaruan
-    Serial.println("Sistem OFF");
-    delay(1000);
+        digitalWrite(trigPin, LOW);
+        delayMicroseconds(2);
+        digitalWrite(trigPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trigPin, LOW);   
+        duration = pulseIn(echoPin, HIGH);
+        //KALKULASI JARAK
+        distance = duration*0.034/2;  
+        //KIRIM DATA KE BLYNK
+        Blynk.virtualWrite(V5, distance);   
+        //KIRIM DATA KE LCD BLYNK
+        lcd.print (3, 0, "Makassar012");    
+        lcd.print (1, 1, "Smart Home 101");
+        timer.run();
+        logic();
+        Serial.print("Durasi: ");
+        Serial.println(duration);
+        Serial.print("Jarak: ");
+        Serial.print(distance);
+        Serial.print (" cm");
+        Serial.println (" ");
+        delay(100);
+  }else  {
+        digitalWrite(LEDsystem, LOW); //pembaruan
+        Serial.println("Sistem OFF");
+        delay(1000);
   }
 
 }
 
 
-//====================== Logika Jarak ==============================
 void logic() {
   if ( distance <= 5) {
 
-    Blynk.setProperty(V6, "color", "#FF0000"); //warna merah
-    Blynk.setProperty(V5, "color", "#FF0000"); //warna merah
-    led1.on();
-      digitalWrite(BUZZ, HIGH);
-      digitalWrite(LEDalarm, HIGH);
-      delay(60);
-      digitalWrite(BUZZ, LOW);
-      digitalWrite(LEDalarm, LOW);
-      delay(30);
-    //}
-    Serial.println(" MEPET");
+    Blynk.setProperty(V6, "color", "#FF0000");
+    Blynk.setProperty(V5, "color", "#FF0000");
+    led1.on()
+    digitalWrite(BUZZ, HIGH);
+    digitalWrite(LEDalarm, HIGH);
+    delay(60);
+    digitalWrite(BUZZ, LOW);
+    digitalWrite(LEDalarm, LOW);
+    delay(30);    
+    Serial.println(" BAHAYA");
   }
 
   else if (( distance > 5 ) && ( distance < 15)) {
 
-    Blynk.setProperty(V6, "color", "#FFE600"); // warna kuning
-    Blynk.setProperty(V5, "color", "#FFE600"); // warna kuning
+    Blynk.setProperty(V6, "color", "#FFE600");
+    Blynk.setProperty(V5, "color", "#FFE600");
     led1.on();
-      digitalWrite(BUZZ, HIGH);
-      digitalWrite(LEDalarm, HIGH);
-      delay(400);
-      digitalWrite(BUZZ, LOW);
-      digitalWrite(LEDalarm, LOW);
-      delay(400);
-    //}
-    Serial.println ( " DEKAT");
+    digitalWrite(BUZZ, HIGH);
+    digitalWrite(LEDalarm, HIGH);
+    delay(400);
+    digitalWrite(BUZZ, LOW);
+    digitalWrite(LEDalarm, LOW);
+    delay(400);
+    Serial.println ( " KURANG AMAN");
 
   }
 
   else if ( distance >= 15) {
-    Blynk.setProperty(V6, "color", "#2EFF26"); //warna hijau
-    Blynk.setProperty(V5, "color", "#2EFF26"); //warna hijau
+    Blynk.setProperty(V6, "color", "#2EFF26");
+    Blynk.setProperty(V5, "color", "#2EFF26");
     led1.on();
     delay(200);
     Serial.println ( " JAUH");
 
   }
 }
-//====================================================================
 
-//======================= Kirim Data Sensor ==========================
+
 void sendSensor() {
-  //clear the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
   
-  //Set the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);    
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  
-  //Read the echoPin, return to sound wave travel time to microseconds
-  duration = pulseIn(echoPin, HIGH);
-  
-  //Calculating the distance
-  distance = duration*0.034/2; //s=t*v
-  
-  //kirim data ke blynk
-  Blynk.virtualWrite(V5, distance);   
-
-  //LCD Di Blynk
-  // lcd.print (0, 0, "Makassar012");
-  // lcd.print (0, 1, "Jarak : " + String(distance) + " cm");
-  // lcd.print (0, 1, "Modul IoT 101");
+  digitalWrite(trigPin, LOW);    
+  duration = pulseIn(echoPin, HIGH);    
+  distance = duration*0.034/2;      
 }
-//====================================================================
 
-//================== Bunyi Awal Saat Tersambung ======================
+
 void Startup()  {
 
   for(int i = 0; i <= 3; i++){
