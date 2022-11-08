8167f9eefc7a48ebd9707fbbfd3aacd0ae72f07c
Daeng Coding <59271775+rintwo512@users.noreply.github.com>
2022-11-08 21:01:30 +0800

    Update new code.ino

--- new code.ino
+++ new code.ino
@@ -9,20 +9,26 @@ Untuk cara integrasi dengan apk blynk bisa kirim email ke : ryntooh@gmail.com
 
 */
 
-
-
+// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
+// See the Device Info tab, or Template settings
 #define BLYNK_TEMPLATE_ID "TMPL1ov1xbJZ"
 #define BLYNK_DEVICE_NAME "Smart Home"
-#define BLYNK_AUTH_TOKEN "bG6ROuRQ4qPRS_3TOy3l5__kOvtGR7-4" s
+#define BLYNK_AUTH_TOKEN "bG6ROuRQ4qPRS_3TOy3l5__kOvtGR7-4"
+
+
+// Comment this out to disable prints and save space
 #define BLYNK_PRINT Serial
+
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
 char auth[] = BLYNK_AUTH_TOKEN;
 
+// Your WiFi credentials.
+// Set password to "" for open networks.
 char ssid[] = "SSID";
-char pass[] = "PASSWORD";
+char pass[] = "PASS";
 
 #define trigPin D1
 #define echoPin D2
@@ -36,15 +42,19 @@ BlynkTimer timer;
 WidgetLED led1(V6);
 WidgetLCD lcd(V7);
 
+
 long duration;
 long distance;
 
+
 void setup()
 {
+  // Debug console
   Serial.begin(115200);
- 
-  pinMode(2, OUTPUT); // RELAY A
-  pinMode(0, OUTPUT); // RELAY B
+
+  
+  pinMode(2, OUTPUT);
+  pinMode(0, OUTPUT);
 
   Blynk.begin(auth, ssid, pass);
 
@@ -61,7 +71,7 @@ void setup()
 }
 
 
-BLYNK_WRITE(V0) // PIN LAMPU BLYNK
+BLYNK_WRITE(V0) 
     if (param.asInt() == 1)
     {        
         digitalWrite(2, LOW);
@@ -72,7 +82,7 @@ BLYNK_WRITE(V0) // PIN LAMPU BLYNK
     }
 }
 
-BLYNK_WRITE(V1) //PIN STOP KONTAK BLYNK 
+BLYNK_WRITE(V1)
 {
     if (param.asInt() == 1)
     {      
@@ -84,7 +94,7 @@ BLYNK_WRITE(V1) //PIN STOP KONTAK BLYNK
     }
 }
 
-BLYNK_WRITE(V4) // PIN TOMBOL MONITOR JARAK BLYNK
+BLYNK_WRITE(V4)
 {
     if (param.asInt() == 1)
     {    
@@ -103,51 +113,59 @@ void loop() {
 
   Blynk.run();
   if (digitalRead(LEDsystem) == HIGH)  {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
     
-        digitalWrite(trigPin, LOW);
-        delayMicroseconds(2);
-        digitalWrite(trigPin, HIGH);
-        delayMicroseconds(10);
-        digitalWrite(trigPin, LOW);   
-        duration = pulseIn(echoPin, HIGH);
-        //KALKULASI JARAK
-        distance = duration*0.034/2;  
-        //KIRIM DATA KE BLYNK
-        Blynk.virtualWrite(V5, distance);   
-        //KIRIM DATA KE LCD BLYNK
-        lcd.print (3, 0, "Makassar012");    
-        lcd.print (1, 1, "Smart Home 101");
-        timer.run();
-        logic();
-        Serial.print("Durasi: ");
-        Serial.println(duration);
-        Serial.print("Jarak: ");
-        Serial.print(distance);
-        Serial.print (" cm");
-        Serial.println (" ");
-        delay(100);
-  }else  {
-        digitalWrite(LEDsystem, LOW); //pembaruan
-        Serial.println("Sistem OFF");
-        delay(1000);
+    
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    
+    
+    duration = pulseIn(echoPin, HIGH);
+  
+    
+    distance = duration*0.034/2;
+  
+    
+    Blynk.virtualWrite(V5, distance);   
+
+    
+    lcd.print (3, 0, "Makassar012");
+    lcd.print (1, 1, "Smart Home 101");
+    timer.run();
+    logic();
+
+    Serial.print("Durasi: ");
+    Serial.println(duration);
+    Serial.print("Jarak: ");
+    Serial.print(distance);
+    Serial.print (" cm");
+    Serial.println (" ");
+    delay(100);
+  }
+  else  {
+    digitalWrite(LEDsystem, LOW);
+    Serial.println("Sistem OFF");
+    delay(1000);
   }
 
 }
-
 
 void logic() {
   if ( distance <= 5) {
 
     Blynk.setProperty(V6, "color", "#FF0000");
     Blynk.setProperty(V5, "color", "#FF0000");
-    led1.on()
-    digitalWrite(BUZZ, HIGH);
-    digitalWrite(LEDalarm, HIGH);
-    delay(60);
-    digitalWrite(BUZZ, LOW);
-    digitalWrite(LEDalarm, LOW);
-    delay(30);    
-    Serial.println(" BAHAYA");
+    led1.on();
+      digitalWrite(BUZZ, HIGH);
+      digitalWrite(LEDalarm, HIGH);
+      delay(60);
+      digitalWrite(BUZZ, LOW);
+      digitalWrite(LEDalarm, LOW);
+      delay(30);
+
+    Serial.println(" -5cm");
   }
 
   else if (( distance > 5 ) && ( distance < 15)) {
@@ -155,13 +173,14 @@ void logic() {
     Blynk.setProperty(V6, "color", "#FFE600");
     Blynk.setProperty(V5, "color", "#FFE600");
     led1.on();
-    digitalWrite(BUZZ, HIGH);
-    digitalWrite(LEDalarm, HIGH);
-    delay(400);
-    digitalWrite(BUZZ, LOW);
-    digitalWrite(LEDalarm, LOW);
-    delay(400);
-    Serial.println ( " KURANG AMAN");
+      digitalWrite(BUZZ, HIGH);
+      digitalWrite(LEDalarm, HIGH);
+      delay(400);
+      digitalWrite(BUZZ, LOW);
+      digitalWrite(LEDalarm, LOW);
+      delay(400);
+    
+    Serial.println ( " 6 - 15cm");
 
   }
 
@@ -170,7 +189,7 @@ void logic() {
     Blynk.setProperty(V5, "color", "#2EFF26");
     led1.on();
     delay(200);
-    Serial.println ( " JAUH");
+    Serial.println ( " 15cm keatas");
 
   }
 }
@@ -179,13 +198,20 @@ void logic() {
 void sendSensor() {
   
   digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);    
+  delayMicroseconds(2);
+  
+  
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);    
-  duration = pulseIn(echoPin, HIGH);    
-  distance = duration*0.034/2;      
+  digitalWrite(trigPin, LOW);
+    
+  duration = pulseIn(echoPin, HIGH);
+    
+  distance = duration*0.034/2;
+  
+  Blynk.virtualWrite(V5, distance);     
 }
+
 
 
 void Startup()  {
@@ -206,5 +232,6 @@ void sendDHT(){
   Blynk.virtualWrite(V3, 70);
 
 }
+
 
 
